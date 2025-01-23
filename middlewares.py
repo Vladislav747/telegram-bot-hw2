bf76b1bad88384f7fb7675b10cbdd6b29863c59a
vlad <zippo585@gmail.com>
2025-01-23 21:09:54 +0500

    Added chart for tg

--- middlewares.py
+++ middlewares.py
@@ -5,4 +5,6 @@ from aiogram.types import Message
 class LoggingMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Message, data: dict):
         print(f"Received message: {event.text}")
+        handler_name = handler.__name__
+        print(f"Handler triggered: {handler_name}")
         return await handler(event, data)
